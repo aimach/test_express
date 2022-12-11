1d8159eb2acd5a329ada8f57fe023d67d44f957a
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2022-12-11 14:25:38 +0100

    handle public and private roads with bearer token

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const express = require("express");
-const { hashPassword, verifyPassword } = require("./auth.js");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
 const { validateMovie, validateUser } = require('./validator.js')
 
 const app = express();
@@ -20,23 +20,29 @@ const movieHandlers = require("./movieHandlers");
 const usersHandlers = require('./usersHandlers')
 const loginHandlers = require('./loginHandlers')
 
+// the public routes
+
 // handle movies
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post('/api/movies', validateMovie, movieHandlers.postMovie);
-app.put('/api/movies/:id', validateMovie, movieHandlers.updateMovie);
-app.delete('/api/movies/:id', movieHandlers.deleteMovie);
-
 //handle users
 app.get('/api/users', usersHandlers.getUsers);
 app.get('/api/users/:id', usersHandlers.getUsersById);
 app.post('/api/users', validateUser, hashPassword, usersHandlers.postUser);
-app.put('/api/users/:id', validateUser, usersHandlers.updateUser);
-app.delete('/api/users/:id', usersHandlers.deleteUser);
-
 //handle login
 app.post('/api/login', loginHandlers.getUserByEmailWiythPasswordAndPassToNext, verifyPassword)
 
+
+// the protected routes
+app.use(verifyToken) // authentication wall
+
+// handle movies
+app.post('/api/movies', validateMovie, movieHandlers.postMovie);
+app.put('/api/movies/:id', validateMovie, movieHandlers.updateMovie);
+app.delete('/api/movies/:id', movieHandlers.deleteMovie);
+// handle users
+app.put('/api/users/:id', validateUser, usersHandlers.updateUser);
+app.delete('/api/users/:id', usersHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
--- auth.js
+++ auth.js
@@ -24,14 +24,14 @@ const hashPassword = (req, res, next) => {
 };
 
 const verifyPassword = (req, res) => {
-  const user = req.user;
-  const token = jwt.sign({ sub: user }, process.env.JWT_SECRET, { expiresIn: '3600s' })
+  const playload = { sub: req.user };
+  const token = jwt.sign(playload, process.env.JWT_SECRET, { expiresIn: '3600s' })
   argon2
     .verify(req.user.hashedPassword, req.body.password)
     .then((isVerified) => {
       if (isVerified) {
         delete req.user.hashedPassword;
-        res.status(200).send({ token, user });
+        res.status(200).send({ token, user: req.user });
       } else {
         res.sendStatus(401);
       }
@@ -42,7 +42,26 @@ const verifyPassword = (req, res) => {
     });
 }
 
+const verifyToken = (req, res, next) => {
+  try {
+    const authorizationHeader = req.headers.authorization
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
+    const [type, token] = authorizationHeader.split(" ");
+    if (type !== 'Bearer') {
+      throw new Error("Authorization header has not the 'Bearer' type");
+    }
+    req.playload = jwt.verify(token, process.env.JWT_SECRET)
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+}
+
 module.exports = {
   hashPassword,
-  verifyPassword
+  verifyPassword,
+  verifyToken
 }
